54028ef7e42c8d6de7b6653a51391d1c21ce6068
Pwnkdm <pawanrajkadam@gmail.com>
2022-04-11 13:01:00 +0530

    done

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,15 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import data from '../data.json'
 
 
 const GroceryDetails = ()=>{
 console.log(data)
 
+const [Data, setData] = useState(data);
     return(
         <>
         <h1>Groceries</h1>
         <div className="container">
             {/* map through the data and display the cards */}
+      { Data.map((item) =>{
+          console.log(item.title)
+       return(
+           <>
+           
+           <img src="item.imgURL" alt="logo" />
+        <h2>{item.title}</h2>
+        <p>{item.discount}</p>
+        <h3>{item.sellingPrice}</h3>
+        <p>MRP:<s> {item.mrp}</s></p>
+
+        </>
+       )
+        
+       
+       })
+       
+    }
+      
         </div>
         </>
     )
